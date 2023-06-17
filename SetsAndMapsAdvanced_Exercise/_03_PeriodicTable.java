ad7ab5eaf31f72d4ffed2ea02a1e6af1bb03774a
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-06-17 23:30:02 +0300

    Sets And Maps Advanced

---
+++ SetsAndMapsAdvanced_Exercise/_03_PeriodicTable.java
@@ -0,0 +1,24 @@
+package SetsAndMapsAdvanced_Exercise;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class _03_PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        Set<String> uniqueElements = new TreeSet<>();
+
+        for (int i = 0; i < number; i++) {
+            String[] elements = scanner.nextLine().split("\\s+");
+
+            uniqueElements.addAll(Arrays.asList(elements));
+        }
+
+        uniqueElements.forEach(element -> System.out.print(element + " "));
+    }
+}
